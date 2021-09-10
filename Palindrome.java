33c4dd74ce1067b1931a55362e95a70ab90e75e1
Venkatarun12 <90474601+Venkatarun12@users.noreply.github.com>
2021-09-11 00:55:15 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		int r,sum=0,temp;    
+	 int n=454;
+	  
+	  temp=n;    
+	  while(n>0){    
+	   r=n%10;   
+	   sum=(sum*10)+r;    
+	  n=n/10;    
+		  }    
+		if(temp==sum)    
+	 System.out.println(sum+ "is palindrome number " );    
+	    else    
+	 System.out.println(sum+ "is not palindrome"+sum);    
+	 
+	}
+	}  
